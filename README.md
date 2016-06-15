8b781ae54dc54cecbc010216d83aefdb7d15ed25
Go7hic <gtfx0209@qq.com>
2016-06-15 23:08:47 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # react-native-loading
 react-native-loading
+#### 使用
+
+```js
+import Loading  from 'react-native-loading'
+
+class YourComponent extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      loadingShow: false
+    }
+  }
+  componentWillMount() {
+    // show loading
+    this.setState({
+      loadingShow: true,
+    })
+  }
+   componentDidMount() {
+    // hide loading
+    setTimeout(() => {
+      this.setState({
+        loadingShow: false,
+      })
+    }, 2000)
+  }
+  ...
+  render() {
+    return (
+      <View>
+        ... 
+        <Loading
+          loadingShow={this.state.loadingShow}
+        />  
+      </View>
+    )  
+  }
+  ```
+  
+  #### 效果
+  
+ ![demo.gif](demo.gif) 
+  
+  #### 支持的属性
+  
+- loadingShow (true or false) - isRequired.
+- size - loading 效果的尺寸.
+- color (Number) - loading 效果的颜色.
+- overlayWidth (Number) - 整个 loading 遮罩层的宽度
+- overlayHeight (Number) - 整个 loading 遮罩层的高度
+- overlayColor (String) - 整个 loading 的背景颜色
+- text (String) - loading 的文字说明
+- textColor (String) - loading 的文字颜色
+- textFontSize (Number) - loading 的文字大小
---
+++ index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import {
+  StyleSheet,
+  ActivityIndicatorIOS,
+  View,
+  Text,
+  Platform,
+  ProgressBarAndroid,
+} from 'react-native'
+
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+})
+export default class Loading extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       isLoading: false,
+    }
+  }
+  static propTypes = {
+    loadingShow: React.PropTypes.bool.isRequired,
+    size: React.PropTypes.string,
+    color: React.PropTypes.string,
+    overlayWidth: React.PropTypes.number,
+    overlayHeight: React.PropTypes.number,
+    overlayColor: React.PropTypes.string,
+    text: React.PropTypes.string,
+    textColor: React.PropTypes.string,
+    textFontSize: React.PropTypes.number,
+  }
+  static defaultProps = {
+    isDismissible: false,
+    overlayWidth: 80,
+    overlayHeight: 80,
+    overlayColor: 'rgba(0,0,0,0.6)',
+    size: (Platform.OS === 'ios') ? 'large' : 'SmallInverse',
+    color: (Platform.OS === 'ios') ? 'gray' : 'white',
+    text: 'Loading',
+    textColor: '#eeeeee',
+    textFontSize: 14,
+  }
+
+  render() {
+    let customStyles = StyleSheet.create({
+      overlay: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        backgroundColor: this.props.overlayColor,
+        width: this.props.overlayWidth,
+        height: this.props.overlayHeight,
+      },
+
+      text: {
+        color: this.props.textColor,
+        fontSize: this.props.textFontSize,
+        marginTop: 8,
+      },
+    })
+
+    if (!this.props.loadingShow) {
+      return (<View />)
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={customStyles.overlay}>
+            {(Platform.OS === 'ios') ?
+              <ActivityIndicatorIOS color={this.props.color} size={this.props.size} /> :
+              <ProgressBarAndroid styleAttr={this.props.size} color={this.props.color} />}
+            <Text style={customStyles.text}>{this.props.text}</Text>
+          </View>
+        </View>
+      )
+    }
+  }
+}
